a46c599dca4de2e0679926919ae9ef1497abb534
DIE-4Y <3314023551@qq.com>
2025-05-29 12:00:18 +0800

    mybatis-plus实现获取常用地址

--- Back_End/mybatis-plus-test/test-1/src/main/java/com/itheima/mp/controller/AddressController.java
+++ Back_End/mybatis-plus-test/test-1/src/main/java/com/itheima/mp/controller/AddressController.java
@@ -1,9 +1,19 @@
 package com.itheima.mp.controller;
 
 
+import com.itheima.mp.domain.vo.AddressVO;
+import com.itheima.mp.service.IAddressService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * <p>
@@ -15,6 +25,17 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/address")
+@Slf4j
+@Api(tags = "收货地址相关接口")
+@RequiredArgsConstructor
 public class AddressController {
 
+    private final IAddressService iAddressService;
+
+    @ApiOperation("根据用户id查询收货地址")
+    @GetMapping("/{id}")
+    public AddressVO queryAddressById(@PathVariable Long id){
+        log.info("根据用户id查询收货地址");
+        return iAddressService.queryAddressById(id);
+    }
 }
--- Back_End/mybatis-plus-test/test-1/src/main/java/com/itheima/mp/service/Impl/AddressServiceImpl.java
+++ Back_End/mybatis-plus-test/test-1/src/main/java/com/itheima/mp/service/Impl/AddressServiceImpl.java
@@ -1,10 +1,17 @@
-package com.itheima.mp.service.impl;
+package com.itheima.mp.service.Impl;
 
+import cn.hutool.core.bean.BeanUtil;
+import com.baomidou.mybatisplus.extension.toolkit.Db;
 import com.itheima.mp.domain.po.Address;
+import com.itheima.mp.domain.po.User;
+import com.itheima.mp.domain.vo.AddressVO;
 import com.itheima.mp.mapper.AddressMapper;
 import com.itheima.mp.service.IAddressService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import lombok.val;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * <p>
@@ -17,4 +24,15 @@ import org.springframework.stereotype.Service;
 @Service
 public class AddressServiceImpl extends ServiceImpl<AddressMapper, Address> implements IAddressService {
 
+    @Override
+    public AddressVO queryAddressById(Long id) {
+        User user = Db.lambdaQuery(User.class).eq(User::getId, id).one();
+        if (user == null || user.getStatus() ==  2){
+            throw new RuntimeException("用户状态异常");
+        }
+
+        Address address = lambdaQuery().eq(Address::getUserId, id).eq(Address::getIsDefault, true).one();
+
+        return BeanUtil.copyProperties(address, AddressVO.class);
+    }
 }
