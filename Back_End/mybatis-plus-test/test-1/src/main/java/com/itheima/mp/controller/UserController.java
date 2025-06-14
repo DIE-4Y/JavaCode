7f9247ed4da705275440fb91bf4c254fa3d99e23
DIE-4Y <3314023551@qq.com>
2025-06-14 09:32:14 +0800

    MybatisPlus拦截器添加分页拦截器实现分页功能

--- Back_End/mybatis-plus-test/test-1/src/main/java/com/itheima/mp/controller/UserController.java
+++ Back_End/mybatis-plus-test/test-1/src/main/java/com/itheima/mp/controller/UserController.java
@@ -9,7 +9,6 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import lombok.val;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
